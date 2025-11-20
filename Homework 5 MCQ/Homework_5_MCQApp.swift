8924e554357875a3f2eb7dd581c022b81b61402b
Shiuan-Ye <sychen1023@gmail.com>
2025-11-20 17:26:26 +0800

    Initial Commit

---
+++ Homework 5 MCQ/Homework_5_MCQApp.swift
@@ -0,0 +1,17 @@
+//
+//  Homework_5_MCQApp.swift
+//  Homework 5 MCQ
+//
+//  Created by 陳宣燁 on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct Homework_5_MCQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
